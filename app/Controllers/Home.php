36fd86b10474d8ecf10f661f906d4579862fd3c7
Rufino John <aguilarufino@gmail.com>
2023-11-23 14:07:33 +0800

    added raw sql

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Database\RawSql;
 class Home extends BaseController
 {
     public function index(): string
@@ -15,7 +16,11 @@ class Home extends BaseController
 
         // $query  = $builder->select('id, first_name')->get(); // SELECT id, first_name FROM authors
 
-        $query = $builder->select('id, CONCAT(first_name, " ", last_name) as full_name')->get(); // SELECT id, CONCAT(first_name, " ", last_name) as full_name FROM authors)
+        // $query = $builder->select('id, CONCAT(first_name, " ", last_name) as full_name')->get(); // SELECT id, CONCAT(first_name, " ", last_name) as full_name FROM authors)
+
+        $sql = "CONCAT(first_name, ' ', last_name) as full_name";
+       $builder->select(new RawSql($sql));
+       $query = $builder->get(); // SELECT CONCAT(first_name, ' ', last_name) as full_name FROM authors
 
 
         $result = $query->getResult(); // array of objects
