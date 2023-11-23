efa2daa08bcde9250fb89904d4f72df78fc88647
Rufino John <aguilarufino@gmail.com>
2023-11-23 14:04:21 +0800

    added fulname

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,7 +11,11 @@ class Home extends BaseController
 
         // $query = $builder->get(); // SELECT * FROM authors
 
-        $query = $builder->getWhere(['id' => 1]); // SELECT * FROM authors WHERE id = 1
+        // $query = $builder->getWhere(['id' => 1]); // SELECT * FROM authors WHERE id = 1
+
+        // $query  = $builder->select('id, first_name')->get(); // SELECT id, first_name FROM authors
+
+        $query = $builder->select('id, CONCAT(first_name, " ", last_name) as full_name')->get(); // SELECT id, CONCAT(first_name, " ", last_name) as full_name FROM authors)
 
 
         $result = $query->getResult(); // array of objects
