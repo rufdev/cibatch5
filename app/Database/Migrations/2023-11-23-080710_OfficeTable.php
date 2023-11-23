f5bca73c92160f49fc8950d60bc1bd436d0d4019
Rufino John <aguilarufino@gmail.com>
2023-11-23 16:17:24 +0800

    added office migration

---
+++ app/Database/Migrations/2023-11-23-080710_OfficeTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class OfficeTable extends Migration
+{
+    public function up()
+    {
+       $fields = [
+              'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => true,
+                'null' => false,
+              ],
+              'code' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+                'null' => false,
+              ],
+              'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+                'null' => false,
+              ],
+              'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+              ],
+              'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+              ],
+         ];
+         $this->forge->addField($fields);
+         $this->forge->addPrimaryKey('id');
+         $this->forge->createTable('offices');
+    }
+
+    public function down()
+    {
+  
+    }
+}
