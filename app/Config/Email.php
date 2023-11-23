f022ced82d4e4a7680486de9e6d0a5f65911f67d
Rufino John <aguilarufino@gmail.com>
2023-11-23 21:21:29 +0800

    added env file

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'aguilarufino@gmail.com';
+    public string $fromName   = 'RUFINO JOHN AGUILAR';
     public string $recipients = '';
 
     /**
