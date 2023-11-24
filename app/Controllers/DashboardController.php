b81a2f6376ad385191629bae2a3002de45dc1987
Rufino John <aguilarufino@gmail.com>
2023-11-24 13:31:44 +0800

    added pages

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -8,6 +8,6 @@ class DashboardController extends BaseController
 {
     public function index()
     {
-        return view('template\admin_template');
+        return view('pages/dashboard');
     }
 }
---
+++ app/Views/pages/dashboard.php
@@ -0,0 +1,13 @@
+<?= $this->extend('template/admin_template') ?>
+
+<?= $this->section('contentarea') ?>
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Dashboard</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection() ?>
