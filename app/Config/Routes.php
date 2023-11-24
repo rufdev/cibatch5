795ffd2660adbde7fbddcc3acab7c518b981deda
Rufino John <aguilarufino@gmail.com>
2023-11-24 09:21:10 +0800

    added restroutes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,19 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+
 $routes->get('/', 'Home::index');
+
+$routes->resource('offices',['controller' => 'OfficeController']);
+
+$routes->resource('tickets',['controller' => 'TicketController']);
+
+
 
 service('auth')->routes($routes);
