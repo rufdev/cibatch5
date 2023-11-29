ace23862331caadfe5b286032d892d9ba93c3ab2
Rufino John <aguilarufino@gmail.com>
2023-11-29 13:29:30 +0800

    added chart example

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -14,6 +14,15 @@ class DashboardController extends BaseController
         $data['totaloffices'] = $officeModel->countAllResults();
         $data['totaltickets'] = $ticketModel->countAllResults();
 
+        $data['barchartdata'] = $officeModel->select("offices.name AS office_name, COUNT(tickets.id) as ticket_count")
+        ->join("tickets", "tickets.office_id = offices.id")
+        ->groupBy("offices.id")
+        ->findAll();
+
+        $data['barchart2data'] = $ticketModel->select("DISTINCT(severity) AS severity, COUNT(id) AS ticket_count")
+        ->groupBy("severity")
+        ->findAll();
+
         return view('pages/dashboard',$data);
     }
 }
--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -12,7 +12,7 @@
 </div>
 <div class="content mb-2">
     <div class="container-fluid">
-        <div class="row">
+        <div class="row mb-2">
             <div class="col-lg-6 col-6">
                 <!-- small box -->
                 <div class="small-box bg-info">
@@ -44,5 +44,135 @@
             </div>
 
         </div>
+        <div class="row mb-2">
+            <div class="col-md-12">
+                <!-- BAR CHART -->
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">Bar Chart</h3>
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="chart">
+                            <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-md-12">
+                <!-- BAR CHART -->
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">Bar Chart 2</h3>
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="chart">
+                            <canvas id="barChart2" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+        </div>
     </div>
     <?= $this->endSection() ?>
+
+    <?= $this->section('pagescript'); ?>
+    <script>
+        $(function() {
+            let barChartCanvas = $('#barChart').get(0).getContext('2d');
+            let barchartdata = <?= json_encode($barchartdata) ?>;
+           
+            let barChartData = {
+                labels: barchartdata.map(function(item) {
+                    return item.office_name;
+                }),
+                datasets: [{
+                    label: "Total Tickets",
+                    backgroundColor: "#28a745",
+                    borderColor: "#28a745",
+                    borderWidth: 1,
+                    data: barchartdata.map(function(item) {
+                        return item.ticket_count;
+                    }),
+                }]
+            }
+
+            new Chart(barChartCanvas, {
+                type: 'bar',
+                data: barChartData,
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    datasetFill: false,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                stepSize: 1
+                            }
+                        }]
+                    }
+                }
+            });
+
+
+            let barChart2Canvas = $('#barChart2').get(0).getContext('2d');
+            let barchart2data = <?= json_encode($barchart2data) ?>;
+           
+            let barChart2Data = {
+                labels: barchart2data.map(function(item) {
+                    return item.severity;
+                }),
+                datasets: [{
+                    label: "Total Tickets",
+                    backgroundColor: "#28a745",
+                    borderColor: "#28a745",
+                    borderWidth: 1,
+                    data: barchart2data.map(function(item) {
+                        return item.ticket_count;
+                    }),
+                }]
+            }
+
+            new Chart(barChart2Canvas, {
+                type: 'bar',
+                data: barChart2Data,
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    datasetFill: false,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                stepSize: 1
+                            }
+                        }]
+                    }
+                }
+            });
+
+        });
+    </script>
+    <?= $this->endSection(); ?>
